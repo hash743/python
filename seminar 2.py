c841e35883c94ffd039c0a28990ca29d7901b5ce
hash743 <91676624+hash743@users.noreply.github.com>
2021-10-07 13:41:56 +0100

    Create seminar 2.py

---
+++ seminar 2.py
@@ -0,0 +1,18 @@
+x1= "Python was created by Guido van Rossum a Dutch programmer."
+x2= "First version of Python 0.9.0 was developed in Centrum Wiskunde & Informatica (CWI) in the Netherlands in 1991."
+x3= "Python 3.0, was released on 3 December 2008."
+
+print(type (x1))
+print(type (x2))
+print(type (x3))
+
+x1_type = True 
+x2_type = True 
+x3_type = True 
+
+check1 = x1_type and x2_type and x3_type 
+check2 = x1_type == x2_type == x3_type 
+print(check1)
+print(check2)
+
+print(x1,x2,x3)
